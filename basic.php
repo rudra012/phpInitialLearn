161bc468db7b43cf81739f3f78f8ff56be12cf33
Shailesh Rudra <shailesh@peerbits.com>
2016-07-12 17:49:58 +0530

    Added All files

--- basic.php
+++ basic.php
@@ -3,7 +3,48 @@
 <body>
 
 <?php
-echo "My first PHP script!";
+
+echo "My first PHP script!<br>";
+echo '--------------------------------------------------------------------<br>';
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x . "<br>";
+echo '--------------------------------------------------------------------<br>';
+
+// all keywords (e.g. if, else, while, echo, etc.), classes, functions,
+// and user-defined functions are NOT case-sensitive.
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+
+echo '--------------------------------------------------------------------<br>';
+
+//However; all variable names are case-sensitive.
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+
+echo '--------------------------------------------------------------------<br>';
+$txt = "W3Schools.com";
+echo "I love $txt!" . "<br>";
+echo '--------------------------------------------------------------------<br>';
+$x = 5;
+$y = 4;
+echo $x + $y. "<br>";
+echo '--------------------------------------------------------------------<br>';
+
 ?>
 
 </body>
--- variableScope.php
+++ variableScope.php
@@ -1,7 +1,82 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by Shailsh Rudra.
  * User: root
  * Date: 12/7/16
  * Time: 5:11 PM
  */
+
+$x = 5; // global scope
+
+function myTest1()
+{
+    // using x inside this function will generate an error
+    echo "<p>Variable x inside function is: $x</p>";
+}
+
+echo '--------------------------------------------------------------------<br>';
+
+myTest1();
+
+echo "<p>Variable x outside function is: $x</p>";
+echo '--------------------------------------------------------------------<br>';
+
+function myTest2()
+{
+    $y = 5; // local scope
+    echo "<p>Variable y inside function is: $y</p>";
+}
+
+myTest2();
+
+
+// using y outside the function will generate an error
+echo "<p>Variable y outside function is: $y</p>";
+
+echo '--------------------------------------------------------------------<br>';
+
+$a = 5;
+$b = 10;
+
+function myTest3()
+{
+    global $a, $b;
+    $b = $a + $b;
+}
+
+myTest3();
+echo "Global variable b after sum: " . $b . "<br>";
+
+echo '--------------------------------------------------------------------<br>';
+$a = 5;
+$b = 10;
+$c = 0;
+
+function myTest4() {
+    $GLOBALS['b'] = $GLOBALS['a'] + $GLOBALS['b'];
+}
+
+myTest4();
+echo "Global variable b after sum: " . $b . "<br>";
+
+echo '--------------------------------------------------------------------<br>';
+
+//when a function is completed/executed, all of its variables are deleted. 
+function myTest() {
+    static $g = 0;
+    $g++;
+    echo "g = ". $g;
+
+    $y = 0;
+    $y++;
+    echo ", y = " . $y. "<br>";
+}
+
+myTest();
+myTest();
+myTest();
+
+//Note: The static variable is still local to the function.
+echo "g = ". $g;
+
+?>

c04d13fefdb657116dea59fce1bfdae303739b9d
Shailesh Rudra <shailesh@peerbits.com>
2016-07-12 17:58:52 +0530

    echo and print difference

---
+++ echoPrint.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by Shailesh.
+ * User: root
+ * Date: 12/7/16
+ * Time: 5:53 PM
+ */
+echo '--------------------------------------------------------------------<br>';
+
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters." . "<br>";
+echo '--------------------------------------------------------------------<br>';
+
+$txt1 = "Learn PHP";
+$txt2 = "github.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>$txt1</h2>";
+echo "Study PHP at $txt2<br>";
+echo $x + $y,"<br>";
+echo '--------------------------------------------------------------------<br>';
+
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>" . "<br>";
+print "I'm about to learn PHP!"."<br>";
+echo '--------------------------------------------------------------------<br>';
+
+?>
